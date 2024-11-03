36f87e65af2cb96476999ffd3a4f54dcf0247754
takanaa <137718540+takanaa@users.noreply.github.com>
2024-11-03 23:31:17 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/lab1.py
@@ -0,0 +1,15 @@
+def tofind(ls, item):
+    for i in range(len(ls)):
+        if ls[i] == item:
+            return i
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = tofind(items_list,find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ Лабораторная работа 3/lab2.py
@@ -0,0 +1,12 @@
+def find_common_participants(fg,sg, sep: str = ","):
+    fset = set(fg.split(sep))
+    sset = set(sg.split(sep))
+    both = fset.intersection(sset)
+    finlist = list(sorted(both))
+    return finlist
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print(find_common_participants(participants_first_group, participants_second_group))
