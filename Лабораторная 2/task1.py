7405c60ad14f9eed030307d9de536272815432a4
takanaa <137718540+takanaa@users.noreply.github.com>
2025-01-19 23:23:12 +0300

    Лабораторная 2 -- ООП

---
+++ Лабораторная 2/task1.py
@@ -0,0 +1,42 @@
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f"Книга '{self.name}'"
+
+    def __repr__(self):
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+
+
+class Library:
+    def __init__(self, books=None):
+        self.books = books if books is not None else []
+
+    def get_next_book_id(self):
+        if not self.books:
+            return 1
+        else:
+            return self.books[-1].id + 1
+
+    def get_index_by_book_id(self, book_id):
+        for index, book in enumerate(self.books):
+            if book.id == book_id:
+                return index
+        raise ValueError("There is no book with this id. Try another")
+
+# Пример использования
+if __name__ == "__main__":
+    library = Library()
+    book1 = Book(id_=library.get_next_book_id(), name='451 градус по Фаренгейту', pages=222) #id == 1, ind == 0
+    library.books.append(book1)
+
+    book2 = Book(id_=library.get_next_book_id(), name='Цветы для Элджернона', pages=5454) #id == 2, ind == 1
+    library.books.append(book2)
+
+    print(library.books)  # Вывод списка книг
+    print(library.get_index_by_book_id(1))  # Индекс книги с id=1
+    print(library.get_index_by_book_id(2))
+    print(library.get_index_by_book_id(3)) #id == 3, ind == None
